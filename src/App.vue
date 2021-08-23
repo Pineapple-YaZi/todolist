34f12b2bb5c50e9936ecdf8989e6394fb885d668
yaya <2080625962@qq.com>
2021-08-24 00:31:23 +0800

    '完成了页面初始化'

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
-  <div>
-
+  <div class="todoapp">
+    <TodoHeader></TodoHeader>
+    <TodoMain></TodoMain>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
+// 导入组件
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoMain from "./components/TodoMain.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+// 导入样式
+import './styles/base.css';
+import './styles/index.css'
+
 export default {
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
+  },
+  // 定义标签
+  components: {
+    TodoHeader,
+    TodoMain,
+    TodoFooter,
   },
 
-  created () {
+  created() {},
 
-  },
-
-  methods: {
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style scoped>
-
 </style>
